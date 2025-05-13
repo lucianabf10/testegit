220dda11424c8cebca4e0baa8a831a9f4d270aa2
Luciana <lucianabf10@gmail.com>
2025-05-13 14:37:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: 'Olá mundo estou funcionando'});
 })
 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Oi eu sou a nova rota'});
+})
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
